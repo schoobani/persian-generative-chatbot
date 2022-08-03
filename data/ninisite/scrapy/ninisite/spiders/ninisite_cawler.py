0cf5f1e0ed2ab8d59bf255f394e661f16143e81b
Mohammad <anvaari@pm.me>
2022-08-03 18:22:28 +0430

    Update ninisite_crawler.py
    Enhancement
    - All item striped so extra white space will be removed
    Bug fix
    - getall shold use in extracting question_text. It fixed

--- data/ninisite/scrapy/ninisite/spiders/ninisite_cawler.py
+++ data/ninisite/scrapy/ninisite/spiders/ninisite_cawler.py
@@ -28,8 +28,11 @@ class NinisiteCawlerSpider(scrapy.Spider):
         topic = response.xpath("//h1[@itemprop='headline']/a/text()").get()
         if topic is None :
             self.logger.error(f"topic in {response.url} is None")
+        else:
+            topic = topic.strip()
         
         main_question = response.xpath("//article[@id='topic']//div[contains(@class,'post-message')]/p/text()").getall()
+        main_question = list(map(lambda x:x.strip(),main_question))
         
         articles = response.xpath("//div[@id='posts']/article")
         
@@ -41,20 +44,17 @@ class NinisiteCawlerSpider(scrapy.Spider):
         
     def get_article_info(self,main_question,topic,article_selector):
         answer = article_selector.xpath(".//div[contains(@class,'post-message')]/p/text()").getall()
+        answer = list(map(lambda x:x.strip(),answer))
         
         reply_selector = article_selector.xpath(".//div[@class='reply-message']")
         if len(reply_selector) != 0:
-            question_text = reply_selector.xpath("./text()").get()
+            question_text = reply_selector.xpath("./text()").get().strip()
             if len(question_text) > 90:
                 question_id = reply_selector.attrib['data-id']
-                question_text = article_selector.xpath(f"//article[@id='post-{question_id}']//div[contains(@class,'post-message')]/p/text()").get()
+                
+                question_text = article_selector.xpath(f"//article[@id='post-{question_id}']//div[contains(@class,'post-message')]/p/text()").getall()
+                question_text = list(map(lambda x:x.strip(),question_text))
         else:
             question_text = main_question
         
         return NinisiteItem(topic,question_text , answer)
-    
-# PRoblem : len(question_text) >90 doesn't work.
-                
-                
-        
-        
